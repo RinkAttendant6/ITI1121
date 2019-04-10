b4ec2dc872789abdbc13421107a267445f43878c
Vincent <vincent.diep61@gmail.com>
2019-04-10 01:08:59 -0400

    Make A1Q3 generic
    implement the getLongestRun method to be able to take an array of any comparable objects
    the method will throw a runtime error if the array is of non-comparable objects

--- Assignments/Assignment 1/01/A1Q3.java
+++ Assignments/Assignment 1/01/A1Q3.java
@@ -1,62 +1,33 @@
-
-//It checks the longest run of a number in an array.
-
-public class A1Q3{
-  public static int getLongestRun(double[] array){
-    int longest=1;
-    int current=1;
-    int i=1;
-    while(i<array.length){
-      if (array[i]==array[i-1]){
-        current+=1;
-        if (current>longest){
-          longest=current;
-
-        }
-
-      }
-      else
-      {
-        current=1;
-      }
-      i++;
-
-
+/**
+ * It checks the longest run of a number in an array.
+ */
+public class A1Q3
+{
+  public static <T extends Comparable<T>> int getLongestRun(T[] array) {
+    if (array.length == 0) {
+      return 0;
     }
-    if(array.length==0)
-    {return 0;}
-    else
-    {return longest;}
+    
+    int longest = 1;
+    
+    for (int current = 1, i = 1; i < array.length; ++i) {
+      current = array[i].compareTo(array[i-1]) == 0 ? current + 1 : 1;
+      
+      if (current > longest) {
+        longest = current;
+      }
+    }
+    
+    return longest;
   }
-
-
-
-  public static int getLongestRun(String[] array){
-    int longest=1;
-    int current=1;
-    int i=1;
-    while(i<array.length){
-      if (array[i].equals(array[i-1])){
-        current+=1;
-        if (current>longest){
-          longest=current;
-
-        }
-
-      }
-      else
-      {
-        current=1;
-      }
-      i++;
-
-
-    }
-    if(array.length==0)
-    {return 0;}
-    else
-    {return longest;}
-    }
-
-
+  
+  public static int getLongestRun(double[] array) {
+    Double[] arr = Arrays.stream(array).boxed().toArray(Double[]::new);
+    return getLongestRun(arr);
   }
+  
+  public static int getLongestRun(int[] array) {
+    Integer[] arr = Arrays.stream(array).boxed().toArray(Integer[]::new);
+    return getLongestRun(arr);
+  }
+}
